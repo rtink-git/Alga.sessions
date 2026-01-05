10573bdabe00542314488bd2518707a1b21abc69
rtink-git <129110347+rtink-git@users.noreply.github.com>
2026-01-05 18:32:12 +0500

    Release 1.1.5

--- Alga.sessions/obj/Debug/net9.0/Alga.sessions.AssemblyInfo.cs
+++ Alga.sessions/obj/Debug/net9.0/Alga.sessions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alga.sessions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0+9692dd830c1857a1a2af2629dbe4023ed73bd316")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0+2b6163478a5554dabe8d40fc61869ef3671c9a6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("sessions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alga.sessions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
